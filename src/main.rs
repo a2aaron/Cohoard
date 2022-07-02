84441d87ef5461c987b6c4e71d806f3a15041d9c
Aaron Kofsky <aaronko@umich.edu>
2022-07-01 20:26:38 -0400

    Start work on templating using Tera :D

---
+++ src/main.rs
@@ -0,0 +1,75 @@
+use std::{
+    io::Read,
+    path::{Path, PathBuf},
+};
+
+use clap::Parser;
+use serde::Serialize;
+use tera::{Context, Tera};
+
+#[derive(Debug, Clone, Serialize)]
+struct Post {
+    user: String,
+    user_color: String,
+    contents: String,
+}
+
+impl Post {
+    fn new(user: String, contents: String) -> Post {
+        let (user, user_color) = match user.as_str() {
+            "AARON" => ("SCP 7C: Delta Runic Gnar - Skim", "0xFF8200"),
+            "CASSIE" => ("Cassie - 2%", "0x69C97A"),
+            _ => (user.as_str(), "0xFFFFFF"),
+        };
+
+        Post {
+            user: user.to_string(),
+            user_color: user_color.to_string(),
+            contents,
+        }
+    }
+}
+
+#[derive(Debug, clap::Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(long = "in", short)]
+    in_file: Option<PathBuf>,
+    #[clap(long = "out", short)]
+    out_file: Option<PathBuf>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let input = if let Some(path) = args.in_file {
+        std::fs::read_to_string(path).unwrap()
+    } else {
+        let mut string = String::new();
+        std::io::stdin().read_to_string(&mut string).unwrap();
+        string
+    };
+
+    let posts = input
+        .lines()
+        .map(|line| {
+            let split = line.split(": ").collect::<Vec<_>>();
+            let user = split[0].to_string();
+            let contents = split[1].to_string();
+            Post::new(user, contents)
+        })
+        .collect::<Vec<_>>();
+
+    let tera = Tera::new("templates/**/*.html").unwrap();
+
+    let mut context = Context::new();
+    context.insert("posts", &posts);
+
+    let html = tera.render("discord.html", &context).unwrap();
+
+    if let Some(out_path) = args.out_file {
+        std::fs::write(out_path, html).unwrap();
+    } else {
+        println!("{}", html);
+    }
+}
