1baa3756a496beba4b2059369784104567e477d1
Aaron Kofsky <aaronko@umich.edu>
2022-10-31 20:40:48 -0400

    Bump website to version 0.7.0 of Cohoard.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import init, * as cohoard_module from "https://static.witchoflight.com/~a2aaron/cohoard/v0.6.0/cohoard.js";
+import init, * as cohoard_module from "https://static.witchoflight.com/~a2aaron/cohoard/v0.7.0/cohoard.js";
 
 import { ConfigTable } from "./config_table.js"
 import { TemplateControls, DISCORD_BUILTIN } from "./template_controls.js";
